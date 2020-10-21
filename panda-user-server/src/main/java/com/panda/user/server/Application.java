4907337f790b3789108c0c8c846eb1c43487b3a5
panjinli <pan_jinli@163.com>
2020-10-21 21:31:07 +0800

    [NFF] init

---
+++ panda-user-server/src/main/java/com/panda/user/server/Application.java
@@ -0,0 +1,17 @@
+package com.panda.user.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jinli
+ * @date 2020/10/21
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setRegisterShutdownHook(false);
+        springApplication.run(args);
+    }
+}
---
+++ panda-user-server/src/main/java/com/panda/user/server/application/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.panda.user.server.application.service;
+
+/**
+ * @author jinli
+ * @date 2020/10/21
+ */
+public class UserServiceImpl {
+}
