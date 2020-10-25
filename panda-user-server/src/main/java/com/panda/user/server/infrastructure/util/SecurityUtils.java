c896ba72aa43e10a0611a105918c2310ce28e7fb
panjinli <pan_jinli@163.com>
2020-10-25 15:04:54 +0800

    [NFF] md5

--- panda-user-server/src/main/java/com/panda/user/server/infrastructure/util/SecurityUtils.java
+++ panda-user-server/src/main/java/com/panda/user/server/infrastructure/util/SecurityUtils.java
@@ -1,9 +1,7 @@
 package com.panda.user.server.infrastructure.util;
 
 import com.panda.user.server.infrastructure.util.entity.ImageVerificationCode;
-import sun.misc.BASE64Encoder;
-
-import java.security.MessageDigest;
+import org.springframework.util.DigestUtils;
 
 
 /**
@@ -15,23 +13,14 @@ public class SecurityUtils {
     private SecurityUtils() {
     }
     
-    private static final  String MD5 = "MD5";
+    private static final String MD5 = "MD5";
     
-    public  static ImageVerificationCode getImageVerificationCode(int weight,int height){
-        ImageVerificationCode ivc = new ImageVerificationCode(weight,height);
-        return  ivc;
+    public static ImageVerificationCode getImageVerificationCode(int weight, int height) {
+        ImageVerificationCode ivc = new ImageVerificationCode(weight, height);
+        return ivc;
     }
     
-    
-    public  static String encryptByMD5(String plainText ){
-    
-        try {
-            MessageDigest md = MessageDigest.getInstance(MD5);
-            byte[] b1 = md.digest(plainText.getBytes());
-            BASE64Encoder encoder = new BASE64Encoder();
-            return encoder.encode(b1);
-        } catch(Exception e) {
-            return null;
-        }
+    public static String encryptByMD5(String plainText) {
+        return DigestUtils.md5DigestAsHex(plainText.getBytes());
     }
- }
+}
